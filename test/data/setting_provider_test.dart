b48300033fe2e0143571003ba31fa63703c6853c
Benno Richters <b.richters@gmail.com>
2019-11-04 21:02:50 +0100

    added extra unit test

--- test/data/setting_provider_test.dart
+++ test/data/setting_provider_test.dart
@@ -4,9 +4,11 @@ import 'package:toptopdo/data/model/settings.dart';
 import 'package:toptopdo/data/settings_provider.dart';
 
 void main() {
-  test('first nothing then retrieve same as saved', () async {
+  setUp(() {
     SharedPreferences.setMockInitialValues({});
+  });
 
+  test('first nothing then retrieve same as saved', () async {
     SharedPreferencesSettingsProvider p = SharedPreferencesSettingsProvider(
       'url',
       'loginName',
@@ -32,5 +34,30 @@ void main() {
     expect(provided.subcategoryId, 'sub');
     expect(provided.durationId, 'd');
     expect(provided.operatorId, 'o');
+  });
+
+  test('different url does not contain the same settings', () async {
+    SharedPreferencesSettingsProvider p1 = SharedPreferencesSettingsProvider(
+      'url1',
+      'loginName',
+    );
+
+    Settings s = Settings(
+      branchId: 'b',
+      callerId: 'c',
+      categoryId: 'cat',
+      subcategoryId: 'sub',
+      durationId: 'd',
+      operatorId: 'o',
+    );
+
+    await p1.save(s);
+
+    SharedPreferencesSettingsProvider p2 = SharedPreferencesSettingsProvider(
+      'url2',
+      'loginName',
+    );
+
+    expect(await p2.provide(), null);
   });
 }
