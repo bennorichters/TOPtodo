f0b0f787643d439d5d14dd2c751090c5221be50d
Benno Richters <b.richters@gmail.com>
2019-11-10 16:59:23 +0100

    fixed wrong types

--- lib/data/credentials_provider.dart
+++ lib/data/credentials_provider.dart
@@ -36,20 +36,20 @@ class SecureStorageCredentials implements CredentialsProvider {
   @override
   Future<void> save(Credentials credentials) {
     const FlutterSecureStorage storage = FlutterSecureStorage();
-    final Future<String> url = storage.write(
+    final Future<void> url = storage.write(
       key: 'url',
       value: credentials.url,
     );
-    final Future<String> loginName = storage.write(
+    final Future<void> loginName = storage.write(
       key: 'loginName',
       value: credentials.loginName,
     );
-    final Future<String> password = storage.write(
+    final Future<void> password = storage.write(
       key: 'password',
       value: credentials.password,
     );
 
-    return Future.wait(<Future<String>>[
+    return Future.wait(<Future<void>>[
       url,
       loginName,
       password,
