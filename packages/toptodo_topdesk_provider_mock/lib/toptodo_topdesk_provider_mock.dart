e83823bedbc9616da3c44fb2e2bf6b50c5875e86
Benno Richters <b.richters@gmail.com>
2019-11-16 16:45:13 +0100

    added latency to fake topdesk provider

--- packages/toptodo_topdesk_provider_mock/lib/toptodo_topdesk_provider_mock.dart
+++ packages/toptodo_topdesk_provider_mock/lib/toptodo_topdesk_provider_mock.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:toptodo_data/toptodo_data.dart';
+
+const Duration _latency = Duration(milliseconds: 1500);
 
 class FakeTopdeskProvider implements TopdeskProvider {
   @override
@@ -26,15 +29,6 @@ class FakeTopdeskProvider implements TopdeskProvider {
         .where((Branch b) => b.name.toLowerCase().startsWith(swLower));
   }
 
-  Future<List<dynamic>> _readJson(String file) async {
-    // This explicit inclusion of the package name seems necessary.
-    // Unit tests will run without (only using 'json/'), but on
-    // a device that fails.
-    final String content = await rootBundle
-        .loadString('packages/toptodo_topdesk_provider_mock/json/' + file);
-    return json.decode(content);
-  }
-
   @override
   Future<Iterable<Person>> fetchPersons({
     @required String startsWith,
@@ -43,9 +37,17 @@ class FakeTopdeskProvider implements TopdeskProvider {
     final String swLower = startsWith.toLowerCase();
 
     final List<dynamic> response = await _readJson('persons.json');
-    return response.map((dynamic e) => Person.fromJson(e)).where(
-        (Person p) =>
-            (p.branchid == branchId) &&
-            p.name.toLowerCase().startsWith(swLower));
+    return response.map((dynamic e) => Person.fromJson(e)).where((Person p) =>
+        (p.branchid == branchId) && p.name.toLowerCase().startsWith(swLower));
+  }
+
+  Future<List<dynamic>> _readJson(String file) async {
+    // This explicit inclusion of the package name seems necessary.
+    // Unit tests will run without (only using 'json/'), but on
+    // a device that fails.
+    final String content = await rootBundle
+        .loadString('packages/toptodo_topdesk_provider_mock/json/' + file);
+
+    return Future<List<dynamic>>.delayed(_latency, () => json.decode(content));
   }
 }
