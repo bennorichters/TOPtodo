49dc0ad8d3b013ad9e4bfaa92579c2329d0d587e
Benno Richters <b.richters@gmail.com>
2019-11-27 21:11:28 +0100

    setting up test for TdShape

--- test/widgets/td_shapes_test.dart
+++ test/widgets/td_shapes_test.dart
@@ -1,10 +1,23 @@
+import 'dart:math' show pi;
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:toptodo/widgets/td_shapes.dart';
+
+class MockCanvas extends Mock implements Canvas {}
 
 void main() {
   testWidgets('better name needed', (WidgetTester tester) async {
     final TdShape tdShape = TdShape();
 
+    final MockCanvas mockCanvas = MockCanvas();
+    
+    final Rect rect = Offset(-400, -400) & Size(800, 800);
+
+    tdShape.paint(mockCanvas, const Size(400, 425));
+
+    verify(
+        mockCanvas.drawArc(rect, 0, argThat(closeTo(pi / 2, .01)), true, any));
   });
 }
-
