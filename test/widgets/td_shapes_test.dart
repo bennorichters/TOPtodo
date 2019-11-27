0d42f49626ea114f827056728ffe66c3d255e7bb
Benno Richters <b.richters@gmail.com>
2019-11-27 20:32:48 +0100

    setting up test for TdShape

---
+++ test/widgets/td_shapes_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:toptodo/widgets/td_shapes.dart';
+
+void main() {
+  testWidgets('better name needed', (WidgetTester tester) async {
+    final TdShape tdShape = TdShape();
+
+  });
+}
+
