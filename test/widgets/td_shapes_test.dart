f8fe77501cf9f4dda290ee784911d44e29b89e01
Benno Richters <b.richters@gmail.com>
2019-11-30 10:07:39 +0100

    testing pixels instead of mocking

--- test/widgets/td_shapes_test.dart
+++ test/widgets/td_shapes_test.dart
@@ -7,28 +7,38 @@ import 'package:flutter/rendering.dart';
 import 'package:image/image.dart' as img;
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:toptodo/utils/colors.dart';
 import 'package:toptodo/widgets/td_shapes.dart';
 
-class MockCanvas extends Mock implements Canvas {}
-
 void main() {
-
   testWidgets('better name needed', (WidgetTester tester) async {
-    final PictureRecorder recorder = PictureRecorder();
-    final Canvas canvas = Canvas(recorder);
-    final TdShape tdShape = TdShape();
+    await tester.runAsync(() async {
+      final PictureRecorder recorder = PictureRecorder();
+      final Canvas canvas = Canvas(recorder);
+      final TdShape tdShape = TdShape();
 
-    tdShape.paint(canvas, const Size(400, 425));
+      tdShape.paint(canvas, const Size(400, 425));
 
-    final Picture picture = recorder.endRecording();
-    final ui.Image image = await picture.toImage(400, 425);
-    final ByteData bd = await image.toByteData();
-    final List<int> values = bd.buffer.asUint8List();
+      final Picture picture = recorder.endRecording();
+      final ui.Image image = await picture.toImage(400, 425);
+      final ByteData bd = await image.toByteData(
+        format: ui.ImageByteFormat.png,
+      );
+      final List<int> values = bd.buffer.asUint8List();
+      final img.Image myImage = img.decodeImage(values);
 
-    final img.Image myImage = img.decodeImage(values);
-    final int pixel = myImage.getPixel(10, 10);
-    print('${forest100.value} $pixel');
+      expect(_argbIsAbgr(forest100, myImage.getPixel(10, 10)), isTrue);
+    });
   });
 }
+
+bool _argbIsAbgr(Color color, int value) =>
+    (color.alpha == _alphaFromAbgr(value)) &&
+    (color.red == _redFromAbgr(value)) &&
+    (color.green == _greenFromAbgr(value)) &&
+    (color.blue == _blueFromAbgr(value));
+
+int _alphaFromAbgr(int value) => (0xff000000 & value) >> 24;
+int _blueFromAbgr(int value) => (0x00ff0000 & value) >> 16;
+int _greenFromAbgr(int value) => (0x0000ff00 & value) >> 8;
+int _redFromAbgr(int value) => (0x000000ff & value) >> 0;
