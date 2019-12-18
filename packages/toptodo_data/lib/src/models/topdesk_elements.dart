0ffb0c602ebf06d9faa7d7590f840db780c27294
Benno Richters <b.richters@gmail.com>
2019-12-18 21:16:17 +0100

    named parameters

--- packages/toptodo_data/lib/src/models/topdesk_elements.dart
+++ packages/toptodo_data/lib/src/models/topdesk_elements.dart
@@ -1,8 +1,11 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 abstract class TdModel extends Equatable {
-  const TdModel(this.id, this.name)
-      : assert(id != null),
+  const TdModel({
+    @required this.id,
+    @required this.name,
+  })  : assert(id != null),
         assert(name != null);
 
   final String id;
@@ -16,42 +19,64 @@ abstract class TdModel extends Equatable {
 }
 
 class Branch extends TdModel {
-  const Branch(String id, String name) : super(id, name);
+  const Branch({
+    @required String id,
+    @required String name,
+  }) : super(id: id, name: name);
 }
 
 class IncidentDuration extends TdModel {
-  const IncidentDuration(String id, String name) : super(id, name);
+  const IncidentDuration({
+    @required String id,
+    @required String name,
+  }) : super(id: id, name: name);
 }
 
 class Category extends TdModel {
-  const Category(String id, String name) : super(id, name);
+  const Category({
+    @required String id,
+    @required String name,
+  }) : super(id: id, name: name);
 }
 
 class SubCategory extends TdModel {
-  const SubCategory(String id, String name, this.category)
-      : assert(category != null),
-        super(id, name);
+  const SubCategory({
+    @required String id,
+    @required String name,
+    @required this.category,
+  })  : assert(category != null),
+        super(id: id, name: name);
 
   final Category category;
 }
 
 abstract class Person extends TdModel {
-  const Person(String id, String name, this.avatar)
-      : assert(avatar != null),
-        super(id, name);
+  const Person({
+    @required String id,
+    @required String name,
+    @required this.avatar,
+  })  : assert(avatar != null),
+        super(id: id, name: name);
 
   final String avatar;
 }
 
 class Caller extends Person {
-  const Caller(String id, String name, String avatar, this.branch)
-      : assert(branch != null),
-        super(id, name, avatar);
+  const Caller({
+    @required String id,
+    @required String name,
+    @required String avatar,
+    @required this.branch,
+  })  : assert(branch != null),
+        super(id: id, name: name, avatar: avatar);
 
   final Branch branch;
 }
 
 class IncidentOperator extends Person {
-  const IncidentOperator(String id, String name, String avatar)
-      : super(id, name, avatar);
+  const IncidentOperator({
+    @required String id,
+    @required String name,
+    @required String avatar,
+  }) : super(id: id, name: name, avatar: avatar);
 }
