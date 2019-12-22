a818592f72d147da8a51ea04e40581e6ef63b860
Benno Richters <b.richters@gmail.com>
2019-12-22 09:16:58 +0100

    styling

--- lib/screens/settings/widgets/search_field.dart
+++ lib/screens/settings/widgets/search_field.dart
@@ -1,43 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toptodo/utils/colors.dart';
 import 'package:toptodo_data/toptodo_data.dart';
-
-// class SearchField<T extends TdModel> extends StatelessWidget {
-//   const SearchField({
-//     @required this.value,
-//     @required this.label,
-//     @required this.search,
-//     @required this.validationText,
-//   });
-
-//   final T value;
-//   final String label;
-//   final VoidCallback search;
-//   final String validationText;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: <Widget>[
-//         Expanded(
-//           child: TextFormField(
-//             controller: TextEditingController()..text = (value?.name) ?? '',
-//             enabled: true,
-//             decoration: InputDecoration(
-//               labelText: label,
-//             ),
-//             validator: (String textValue) =>
-//                 value == null ? validationText : null,
-//           ),
-//         ),
-//         IconButton(
-//           icon: const Icon(Icons.search),
-//           onPressed: search,
-//         ),
-//       ],
-//     );
-//   }
-// }
 
 class SearchField<T extends TdModel> extends FormField<T> {
   SearchField({
@@ -48,32 +10,26 @@ class SearchField<T extends TdModel> extends FormField<T> {
   }) : super(
           validator: (T _) => value == null ? validationText : null,
           builder: (FormFieldState<T> state) {
-            return Column(
+            return Row(
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text((value?.name) ?? ''),
+                Expanded(
+                  child: InputDecorator(
+                    child: Text((value?.name) ?? ''),
+                    isEmpty: value == null,
+                    decoration: InputDecoration(
+                      labelText: label,
+                      errorText: state.errorText,
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.search),
-                      onPressed: search,
-                    ),
-                  ],
+                  ),
                 ),
-                if (state.hasError)
-                  Text(
-                    state.errorText,
-                    style: TextStyle(color: vermillion),
-                  )
-                else
-                  Container()
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: search,
+                ),
               ],
             );
           },
-        ) {
-    print('SearchField constructor: $value');
-  }
+        );
 
   final T value;
   final String label;
