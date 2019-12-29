d9efcc136e6d57c33e87d2b5cda463389ec06bbd
Benno Richters <b.richters@gmail.com>
2019-12-29 13:47:21 +0100

    yeah! tables for layout

--- lib/screens/init/init_screen.dart
+++ lib/screens/init/init_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:toptodo/blocs/init/bloc.dart';
+import 'package:toptodo/utils/colors.dart';
 import 'package:toptodo/widgets/error_dialog.dart';
 
 class InitScreen extends StatefulWidget {
@@ -56,42 +57,64 @@ class _InitDataProgress extends StatelessWidget {
   const _InitDataProgress(this.state);
   final InitData state;
 
+  static const _progressDiameter = 25.0;
+  static const _padding = 10.0;
+  static const _firstColumnWidth = _progressDiameter + 2 * _padding;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Table(
+        columnWidths: {
+          0: FixedColumnWidth(_firstColumnWidth),
+          1: IntrinsicColumnWidth(),
+        },
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              state.credentials == null
-                  ? CircularProgressIndicator()
-                  : Icon(Icons.done),
-              const Text('credentials'),
-            ],
+          TableRow(
+            children: _rowChildren(
+              'credentials',
+              state.credentials,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              state.currentOperator == null
-                  ? CircularProgressIndicator()
-                  : Icon(Icons.done),
-              const Text('your operator profile'),
-            ],
+          TableRow(
+            children: _rowChildren(
+              'settings',
+              state.settings,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              state.settings == null
-                  ? CircularProgressIndicator()
-                  : Icon(Icons.done),
-              const Text('settings'),
-            ],
+          TableRow(
+            children:
+                _rowChildren('your operator profile', state.currentOperator),
           ),
         ],
       ),
     );
+  }
+
+  List<Widget> _rowChildren(String text, Object objectToLoad) {
+    return [
+      Padding(
+        padding: const EdgeInsets.all(_padding),
+        child: SizedBox(
+          height: _progressDiameter,
+          child: objectToLoad == null
+              ? CircularProgressIndicator()
+              : Icon(
+                  Icons.done,
+                  color: moss,
+                ),
+        ),
+      ),
+      TableCell(
+        verticalAlignment: TableCellVerticalAlignment.middle,
+        child: Padding(
+          padding: EdgeInsetsDirectional.only(start: _padding),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    ];
   }
 }
