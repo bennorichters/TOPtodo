1bdc4d3a9f254c317d35cc0f18b2abdf048d93ea
Benno Richters <b.richters@gmail.com>
2019-12-30 07:22:40 +0100

    layouting menu dialog

--- lib/widgets/menu_dialog.dart
+++ lib/widgets/menu_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 
 class MenuDialog extends StatelessWidget {
@@ -13,32 +14,61 @@ class MenuDialog extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 8.0, top: 3.0),
-              child: Icon(Icons.close),
+              padding: const EdgeInsets.only(top: 3.0),
+              child: IconButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  icon: const Icon(Icons.close)),
             ),
             Padding(
-              padding: const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.fromLTRB(25, 20, 25, 12),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Icon(Icons.settings),
-                      Text('open settings'),
-                    ],
+                  const _MenuItem(
+                    iconData: Icons.settings,
+                    text: 'settings',
+                    route: 'settings',
                   ),
-                  Row(
-                    children: [
-                      Icon(Icons.person),
-                      Text('log out'),
-                    ],
+                  SizedBox(height: 20),
+                  const _MenuItem(
+                    iconData: Icons.person,
+                    text: 'log out',
+                    route: 'login',
                   ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _MenuItem extends StatelessWidget {
+  const _MenuItem({
+    @required this.iconData,
+    @required this.text,
+    @required this.route,
+    Key key,
+  }) : super(key: key);
+  final IconData iconData;
+  final String text;
+  final String route;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.pushReplacementNamed(context, route),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(iconData),
+          ),
+          Text(text),
+        ],
       ),
     );
   }
