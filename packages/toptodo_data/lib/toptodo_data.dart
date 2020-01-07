bbb2e939668418a1fd24b4993ae52a9f37abb175
Benno Richters <bennor@topdesk.com>
2020-01-07 10:41:17 +0100

    export exeptions

--- packages/toptodo_data/lib/toptodo_data.dart
+++ packages/toptodo_data/lib/toptodo_data.dart
@@ -5,3 +5,4 @@ export './src/models/credentials.dart';
 export './src/models/settings.dart';
 export './src/models/topdesk_elements.dart';
 export './src/providers/repository_providers_api.dart';
+export './src/td_exceptions/td_exceptions.dart';
