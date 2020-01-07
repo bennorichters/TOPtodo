060faeb679809433036061d0f303c4acc4359706
Benno Richters <bennor@topdesk.com>
2020-01-07 07:54:23 +0100

    Dart doc

--- packages/toptodo_data/lib/src/providers/repository_providers_api.dart
+++ packages/toptodo_data/lib/src/providers/repository_providers_api.dart
@@ -4,27 +4,55 @@ import 'package:toptodo_data/src/models/credentials.dart';
 import 'package:toptodo_data/src/models/settings.dart';
 import 'package:toptodo_data/src/models/topdesk_elements.dart';
 
+/// Provider for [Credentials]
 abstract class CredentialsProvider {
+  /// Returns a per user unique instance of a [Credentials]
   Future<Credentials> provide();
+
+  /// Saves the user's [Credentials]
   Future<void> save(Credentials credentials);
+
+  /// Deletes the user's [Credentials]
   Future<void> delete();
 }
 
+/// Provider for [Settings]
 abstract class SettingsProvider {
+  /// Initialiazes this provider.
   void init(String url, String loginName);
+
+  /// Invalidates a previous call to init. Implementations can use this method
+  /// to  dispose of any cached data.
   void dispose();
+
+  /// Returns a per user and per combination of values used to [init()] this
+  /// provider [Settings]
   Future<Settings> provide();
+
+  /// Saves the user's [Settings] for the combination of values used to
+  /// [init()] this provider
   Future<void> save(Settings settings);
+
+  /// Deletes the user's [Settings] for the combination of values used to
+  /// [init()] this provider
   Future<void> delete();
 }
 
+/// Provider for TOPdesk elements.
 abstract class TopdeskProvider {
+  /// Initializes this provider with the given [Credentials]
   void init(Credentials credentials);
+
+  /// Invalidates a previous call to init. Implementations can use this method
+  /// to  dispose of any cached data.
   void dispose();
 
+  /// Returns the [TdBranch] with the given `id`
   Future<TdBranch> tdBranch({
     @required String id,
   });
+  /// Returns the first x [TdBranch]es which names start with `startsWith`, 
+  /// where x depends on the implementation.
   Future<Iterable<TdBranch>> tdBranches({
     @required String startsWith,
   });
@@ -62,7 +90,7 @@ abstract class TopdeskProvider {
     @required String startsWith,
   });
 
-  Future<String> createIncident({
+  Future<String> createTdIncident({
     @required String briefDescription,
     @required Settings settings,
     String request,
