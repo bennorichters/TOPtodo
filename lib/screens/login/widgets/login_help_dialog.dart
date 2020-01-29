b89d73004289d07ee2b3c3a5e14c1cd12096d8f6
Benno Richters <bennor@topdesk.com>
2020-01-29 14:19:09 +0100

    max size for login help box

--- lib/screens/login/widgets/login_help_dialog.dart
+++ lib/screens/login/widgets/login_help_dialog.dart
@@ -10,45 +10,49 @@ class LoginHelpDialog extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          DialogHeader(),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
-            child: RichText(
-              text: TextSpan(
-                style: TextStyle(color: Colors.black),
-                children: [
-                  TextSpan(
-                      text: 'Fill in the TOPdesk address and your credentials. '
-                          'Note that the '),
-                  TextSpan(
-                    text: 'application password ',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                  TextSpan(
-                    text: 'differs from your normal password. Read the ',
-                  ),
-                  TextSpan(
-                    text: 'online documentation ',
-                    style: TextStyle(color: Colors.blue),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        launch(
-                          'https://bennorichters.github.io/TOPtodo/',
-                        );
-                      },
-                  ),
-                  TextSpan(
-                    text: 'for more details.',
-                  ),
-                ],
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 400),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DialogHeader(),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                        text:
+                            'Fill in the TOPdesk address and your credentials. '
+                            'Note that the '),
+                    TextSpan(
+                      text: 'application password ',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                    TextSpan(
+                      text: 'differs from your normal password. Read the ',
+                    ),
+                    TextSpan(
+                      text: 'online documentation ',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          launch(
+                            'https://bennorichters.github.io/TOPtodo/',
+                          );
+                        },
+                    ),
+                    TextSpan(
+                      text: 'for more details.',
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
