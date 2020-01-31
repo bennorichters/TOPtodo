1d7eaa1b470a9bbec7b048f951ac4315410ba30e
Benno Richters <bennor@topdesk.com>
2020-01-31 14:40:30 +0100

    more tests

--- packages/toptodo_backend/test/server_test.dart
+++ packages/toptodo_backend/test/server_test.dart
@@ -14,7 +14,7 @@ void main() {
     final client = MockClient((Request request) async {
       final mockHttpRequest = MockHttpRequest(request.method, request.url);
 
-      requestHandler(mockHttpRequest);
+      await requestHandler(mockHttpRequest);
 
       final response = mockHttpRequest.response;
       final body = await response.transform(utf8.decoder).join();
@@ -40,137 +40,195 @@ void main() {
       apiTdProvider.dispose();
     });
 
-    test('version', () async {
-      expect(
-        await apiTdProvider.apiVersion(),
-        await fakeTdProvider.apiVersion(),
-      );
+    group('get', () {
+      test('version', () async {
+        expect(
+          await apiTdProvider.apiVersion(),
+          await fakeTdProvider.apiVersion(),
+        );
+      });
+
+      test('branch by id', () async {
+        expect(
+          await apiTdProvider.tdBranch(id: 'a'),
+          await fakeTdProvider.tdBranch(id: 'a'),
+        );
+      });
+
+      test('all branches', () async {
+        expect(
+          await apiTdProvider.tdBranches(startsWith: ''),
+          await fakeTdProvider.tdBranches(startsWith: ''),
+        );
+      });
+
+      test('branches startswith TOP', () async {
+        expect(
+          await apiTdProvider.tdBranches(startsWith: 'TOP'),
+          await fakeTdProvider.tdBranches(startsWith: 'TOP'),
+        );
+      });
+
+      test('caller by id', () async {
+        expect(
+          await apiTdProvider.tdCaller(id: 'aa'),
+          await fakeTdProvider.tdCaller(id: 'aa'),
+        );
+      });
+
+      test('all callers from branch a for now returns all callers', () async {
+        final branchA = await apiTdProvider.tdBranch(id: 'a');
+        final allCallers = await apiTdProvider.tdCallers(
+          tdBranch: branchA,
+          startsWith: '',
+        );
+        final callersBranchA = await fakeTdProvider.tdCallers(
+          tdBranch: branchA,
+          startsWith: '',
+        );
+
+        expect(
+          callersBranchA.every((c) => allCallers.contains(c)),
+          isTrue,
+        );
+      });
+
+      test('category by id', () async {
+        expect(
+          await apiTdProvider.tdCategory(id: 'a'),
+          await fakeTdProvider.tdCategory(id: 'a'),
+        );
+      });
+
+      test('all categories', () async {
+        expect(
+          await apiTdProvider.tdCategories(),
+          await fakeTdProvider.tdCategories(),
+        );
+      });
+
+      test('subcategory by id', () async {
+        expect(
+          await apiTdProvider.tdSubcategory(id: 'aa'),
+          await fakeTdProvider.tdSubcategory(id: 'aa'),
+        );
+      });
+
+      test('all subcategories', () async {
+        final cat = await apiTdProvider.tdCategory(id: 'a');
+        expect(
+          await apiTdProvider.tdSubcategories(tdCategory: cat),
+          await fakeTdProvider.tdSubcategories(tdCategory: cat),
+        );
+      });
+
+      test('duration by id', () async {
+        expect(
+          await apiTdProvider.tdDuration(id: 'a'),
+          await fakeTdProvider.tdDuration(id: 'a'),
+        );
+      });
+
+      test('all durations', () async {
+        expect(
+          await apiTdProvider.tdDurations(),
+          await fakeTdProvider.tdDurations(),
+        );
+      });
+
+      test('operator by id', () async {
+        expect(
+          await apiTdProvider.tdOperator(id: 'a'),
+          await fakeTdProvider.tdOperator(id: 'a'),
+        );
+      });
+
+      test('all operators', () async {
+        expect(
+          await apiTdProvider.tdOperators(startsWith: ''),
+          await fakeTdProvider.tdOperators(startsWith: ''),
+        );
+      });
+
+      test('current operator', () async {
+        expect(
+          await apiTdProvider.currentTdOperator(),
+          await fakeTdProvider.currentTdOperator(),
+        );
+      });
     });
 
-    test('branch by id', () async {
-      expect(
-        await apiTdProvider.tdBranch(id: 'a'),
-        await fakeTdProvider.tdBranch(id: 'a'),
+    group('post', () {
+      test('create incident', () async {
+        final settings = Settings(
+          tdBranchId: 'a',
+          tdCallerId: 'aa',
+          tdCategoryId: 'a',
+          tdSubcategoryId: 'aa',
+          tdDurationId: 'a',
+          tdOperatorId: 'a',
+        );
+        final nr = await apiTdProvider.createTdIncident(
+          settings: settings,
+          briefDescription: '',
+        );
+
+        expect(nr, isNotEmpty);
+      });
+    });
+  });
+
+  group('errors', () {
+    test('unsupported method', () async {
+      final mockHttpRequest = MockHttpRequest(
+        'PUT',
+        Uri.http('test.com', '/'),
       );
+
+      await requestHandler(mockHttpRequest);
+      expect(mockHttpRequest.response.statusCode, 405);
     });
 
-    test('all branches', () async {
-      expect(
-        await apiTdProvider.tdBranches(startsWith: ''),
-        await fakeTdProvider.tdBranches(startsWith: ''),
+    test('404 get tas/api', () async {
+      final mockHttpRequest = MockHttpRequest(
+        'GET',
+        Uri.http('test.com', '/tas/api/doesnotexist'),
       );
+
+      await requestHandler(mockHttpRequest);
+      expect(mockHttpRequest.response.statusCode, 404);
     });
 
-    test('branches startswith TOP', () async {
-      expect(
-        await apiTdProvider.tdBranches(startsWith: 'TOP'),
-        await fakeTdProvider.tdBranches(startsWith: 'TOP'),
+    test('404 get different', () async {
+      final mockHttpRequest = MockHttpRequest(
+        'GET',
+        Uri.http('test.com', '/doesnotexist'),
       );
+
+      await requestHandler(mockHttpRequest);
+      expect(mockHttpRequest.response.statusCode, 404);
     });
 
-    test('caller by id', () async {
-      expect(
-        await apiTdProvider.tdCaller(id: 'aa'),
-        await fakeTdProvider.tdCaller(id: 'aa'),
+    test('404 post', () async {
+      final mockHttpRequest = MockHttpRequest(
+        'POST',
+        Uri.http('test.com', 'doesnotexist'),
       );
+
+      await requestHandler(mockHttpRequest);
+      expect(mockHttpRequest.response.statusCode, 404);
     });
+  });
 
-    test('all callers from branch a for now returns all callers', () async {
-      final branchA = await apiTdProvider.tdBranch(id: 'a');
-      final allCallers = await apiTdProvider.tdCallers(
-        tdBranch: branchA,
-        startsWith: '',
-      );
-      final callersBranchA = await fakeTdProvider.tdCallers(
-        tdBranch: branchA,
-        startsWith: '',
+  group('other', () {
+    test('root', () async {
+      final mockHttpRequest = MockHttpRequest(
+        'GET',
+        Uri.http('test.com', '/'),
       );
 
-      expect(
-        callersBranchA.every((c) => allCallers.contains(c)),
-        isTrue,
-      );
-    });
-
-    test('category by id', () async {
-      expect(
-        await apiTdProvider.tdCategory(id: 'a'),
-        await fakeTdProvider.tdCategory(id: 'a'),
-      );
-    });
-
-    test('all categories', () async {
-      expect(
-        await apiTdProvider.tdCategories(),
-        await fakeTdProvider.tdCategories(),
-      );
-    });
-
-    test('subcategory by id', () async {
-      expect(
-        await apiTdProvider.tdSubcategory(id: 'aa'),
-        await fakeTdProvider.tdSubcategory(id: 'aa'),
-      );
-    });
-
-    test('all subcategories', () async {
-      final cat = await apiTdProvider.tdCategory(id: 'a');
-      expect(
-        await apiTdProvider.tdSubcategories(tdCategory: cat),
-        await fakeTdProvider.tdSubcategories(tdCategory: cat),
-      );
-    });
-
-    test('duration by id', () async {
-      expect(
-        await apiTdProvider.tdDuration(id: 'a'),
-        await fakeTdProvider.tdDuration(id: 'a'),
-      );
-    });
-
-    test('all durations', () async {
-      expect(
-        await apiTdProvider.tdDurations(),
-        await fakeTdProvider.tdDurations(),
-      );
-    });
-
-    test('operator by id', () async {
-      expect(
-        await apiTdProvider.tdOperator(id: 'a'),
-        await fakeTdProvider.tdOperator(id: 'a'),
-      );
-    });
-
-    test('all operators', () async {
-      expect(
-        await apiTdProvider.tdOperators(startsWith: ''),
-        await fakeTdProvider.tdOperators(startsWith: ''),
-      );
-    });
-
-    test('current operator', () async {
-      expect(
-        await apiTdProvider.currentTdOperator(),
-        await fakeTdProvider.currentTdOperator(),
-      );
-    });
-
-    test('create incident', () async {
-      final settings = Settings(
-        tdBranchId: 'a',
-        tdCallerId: 'aa',
-        tdCategoryId: 'a',
-        tdSubcategoryId: 'aa',
-        tdDurationId: 'a',
-        tdOperatorId: 'a',
-      );
-      final nr = await apiTdProvider.createTdIncident(
-        settings: settings,
-        briefDescription: '',
-      );
-
-      expect(nr, isNotEmpty);
+      await requestHandler(mockHttpRequest);
+      expect(mockHttpRequest.response.statusCode, 200);
     });
   });
 }
