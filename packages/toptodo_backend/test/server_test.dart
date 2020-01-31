6002887a8482451c104ff1ba2cf490c7a9c082eb
Benno Richters <b.richters@gmail.com>
2020-01-31 07:50:43 +0100

    compare to mock provider

--- packages/toptodo_backend/test/server_test.dart
+++ packages/toptodo_backend/test/server_test.dart
@@ -6,6 +6,7 @@ import 'package:mock_request/mock_request.dart';
 import 'package:test/test.dart';
 import 'package:toptodo_data/toptodo_data.dart';
 import 'package:toptodo_topdesk_provider_api/toptodo_topdesk_provider_api.dart';
+import 'package:toptodo_topdesk_provider_mock/toptodo_topdesk_provider_mock.dart';
 
 import '../bin/server.dart';
 
@@ -29,23 +30,36 @@ void main() {
       password: 'secret',
     );
 
-    final tdProvider = ApiTopdeskProvider();
+    final apiTdProvider = ApiTopdeskProvider();
+    final fakeTdProvider = FakeTopdeskProvider(latency: Duration.zero);
 
     setUp(() async {
-      await tdProvider.init(credentials, client: client);
+      await apiTdProvider.init(credentials, client: client);
     });
 
     tearDown(() {
-      tdProvider.dispose();
+      apiTdProvider.dispose();
     });
 
     test('version', () async {
-      expect(await tdProvider.apiVersion(), '3.1.0');
+      expect(
+        await apiTdProvider.apiVersion(),
+        await fakeTdProvider.apiVersion(),
+      );
+    });
+
+    test('branch by id', () async {
+      expect(
+        await apiTdProvider.tdBranch(id: 'a'),
+        await fakeTdProvider.tdBranch(id: 'a'),
+      );
     });
 
     test('current operator', () async {
-      final currentOperator = await tdProvider.currentTdOperator();
-      expect(currentOperator.name, 'Dawn Meadows');
+      expect(
+        await apiTdProvider.currentTdOperator(),
+        await fakeTdProvider.currentTdOperator(),
+      );
     });
   });
 }
