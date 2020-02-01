57d877dceda00a6897cb6a0b07d1cc04388b433e
Benno Richters <bennor@topdesk.com>
2020-02-01 09:23:28 +0100

    renamed packages

--- scripts/update.dart
+++ scripts/update.dart
@@ -4,14 +4,15 @@ void main() {
   final currentDir = Directory.current.path;
 
   upgrade(currentDir, 'toptodo_data');
-  upgrade(currentDir, 'toptodo_repository_providers_impl');
-  upgrade(currentDir, 'toptodo_topdesk_provider_api');
-  upgrade(currentDir, 'toptodo_topdesk_provider_mock');
+  upgrade(currentDir, 'toptodo_local_storage');
+  upgrade(currentDir, 'toptodo_topdesk_api');
+  upgrade(currentDir, 'toptodo_topdesk_test_data');
+  upgrade(currentDir, 'toptodo_backend');
 }
 
 void upgrade(String currentDir, String packageName) {
   Directory.current = Directory('$currentDir/packages/$packageName');
-  Process.run('pub', <String>['upgrade']).then((ProcessResult pr) {
+  Process.run('pub', ['upgrade']).then((ProcessResult pr) {
     print(pr.exitCode);
     print(pr.stdout);
     print(pr.stderr);
