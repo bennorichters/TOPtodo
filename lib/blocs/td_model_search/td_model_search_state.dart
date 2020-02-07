e58e4eaa6a1f4c40f867f022b2752c368ab050d9
Benno Richters <bennor@topdesk.com>
2020-02-07 08:10:30 +0100

    dartdoc

--- lib/blocs/td_model_search/td_model_search_state.dart
+++ lib/blocs/td_model_search/td_model_search_state.dart
@@ -1,23 +1,33 @@
 import 'package:equatable/equatable.dart';
 import 'package:toptodo_data/toptodo_data.dart';
 
+/// Base class for all search related states
 abstract class TdModelSearchState extends Equatable {
+  /// Creates an instance of [TdModelSearchState]
   const TdModelSearchState();
 
   @override
   List<Object> get props => [];
 }
 
+/// State emitted when the bloc is first initialized
 class TdModelSearchInitialState extends TdModelSearchState {
+  /// Creates an instance of [TdModelSearchInitialState]
   const TdModelSearchInitialState();
 }
 
+/// State emitted when the bloc started a search and is waiting for results.
 class TdModelSearching extends TdModelSearchState {
+  /// Creates an instance of [TdModelSearching]
   const TdModelSearching();
 }
 
+/// State emitted when the bloc received search results
 class TdModelSearchResults<T extends TdModel> extends TdModelSearchState {
+  /// Creates an instance of [TdModelSearchResults]
   const TdModelSearchResults(this.results);
+
+  /// the search results
   final Iterable<T> results;
 
   @override
