693aacbd54023ce79daa0f7c65bbe531c33bdc56
Benno Richters <b.richters@gmail.com>
2020-02-11 20:51:12 +0100

    new test

---
+++ test/screens/init/widgets/init_data_progress_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:toptodo/blocs/init/bloc.dart';
+import 'package:toptodo/screens/init/widgets/init_data_progress.dart';
+
+import '../../../helper.dart';
+
+void main() {
+  group('InitDataProgress', () {
+    testWidgets('no data, 3 CircularProgressIndicators',
+        (WidgetTester tester) async {
+      final data = InitData.empty();
+      await tester.pumpWidget(
+        TestableWidgetWithMediaQuery(
+          width: 600,
+          height: 800,
+          child: InitDataProgress(data),
+        ),
+      );
+
+      expect(find.byType(CircularProgressIndicator), findsNWidgets(3));
+    });
+  });
+}
