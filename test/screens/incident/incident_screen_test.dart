7d75308f970a853bf08d720926c34b5975bc9039
Benno Richters <b.richters@gmail.com>
2020-02-11 16:46:44 +0100

    simplified unit tests

--- test/screens/incident/incident_screen_test.dart
+++ test/screens/incident/incident_screen_test.dart
@@ -1,48 +1,20 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:toptodo/widgets/error_dialog.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:toptodo_data/toptodo_data.dart';
 
 import 'package:toptodo/blocs/incident/bloc.dart';
 import 'package:toptodo/screens/incident/incident_screen.dart';
 import 'package:toptodo/screens/incident/widgets/incident_form.dart';
+import 'package:toptodo/widgets/error_dialog.dart';
 import 'package:toptodo/widgets/menu_operator_button.dart';
 
 import '../../helper.dart';
 
-class _TriggerCreateIncident extends IncidentEvent {}
-
-class _TriggerError extends IncidentEvent {}
-
-class _TriggerOperator extends IncidentEvent {}
-
-class _SimpleIncidentBloc extends IncidentBloc {
-  @override
-  Stream<IncidentState> mapEventToState(IncidentEvent event) async* {
-    if (event is _TriggerCreateIncident) {
-      yield const IncidentCreated(
-        currentOperator: null,
-        number: '123',
-      );
-    } else if (event is _TriggerError) {
-      yield IncidentCreationError(
-        cause: 'just testing',
-        stackTrace: StackTrace.current,
-        currentOperator: null,
-      );
-    } else if (event is _TriggerOperator) {
-      yield const IncidentState(
-        currentOperator: TdOperator(
-          id: 'a',
-          name: 'a',
-          firstLine: true,
-          secondLine: true,
-        ),
-      );
-    }
-  }
-}
+class MockIncidentBloc extends MockBloc<IncidentEvent, IncidentState>
+    implements IncidentBloc {}
 
 void main() {
   group('IncidentScreen', () {
@@ -64,7 +36,7 @@ void main() {
     }
 
     setUp(() {
-      bloc = _SimpleIncidentBloc();
+      bloc = MockIncidentBloc();
     });
 
     tearDown(() {
@@ -72,6 +44,8 @@ void main() {
     });
 
     testWidgets('current operator is null', (WidgetTester tester) async {
+      when(bloc.state).thenReturn(IncidentState(currentOperator: null));
+
       await pumpScreen(tester);
 
       expect(find.byType(MenuOperatorButton), findsNothing);
@@ -79,35 +53,59 @@ void main() {
     });
 
     testWidgets('with current operator', (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        bloc.add(_TriggerOperator());
-        await Future.delayed(Duration(milliseconds: 100));
+      when(bloc.state).thenReturn(IncidentState(
+        currentOperator: TdOperator(
+          id: 'a',
+          name: 'a',
+          firstLine: true,
+          secondLine: true,
+        ),
+      ));
 
-        await pumpScreen(tester);
-        expect(find.byType(MenuOperatorButton), findsOneWidget);
-      });
+      await pumpScreen(tester);
+      expect(find.byType(MenuOperatorButton), findsOneWidget);
     });
 
     testWidgets('show snackbar after incident created', (tester) async {
-      await tester.runAsync(() async {
-        await pumpScreen(tester);
-        bloc.add(_TriggerCreateIncident());
+      final initialState = IncidentState(currentOperator: null);
+      when(bloc.state).thenReturn(initialState);
+      whenListen(
+        bloc,
+        Stream.fromIterable(
+          [
+            initialState,
+            IncidentCreated(number: '123', currentOperator: null),
+          ],
+        ),
+      );
 
-        await Future.delayed(Duration(milliseconds: 100));
-        await tester.pump();
-        expect(find.text('Incident created with number 123'), findsOneWidget);
-      });
+      await pumpScreen(tester);
+
+      await tester.pump();
+      expect(find.text('Incident created with number 123'), findsOneWidget);
     });
 
     testWidgets('show error dialog after error', (tester) async {
-      await tester.runAsync(() async {
-        await pumpScreen(tester);
-        bloc.add(_TriggerError());
+      final initialState = IncidentState(currentOperator: null);
+      when(bloc.state).thenReturn(initialState);
+      whenListen(
+        bloc,
+        Stream.fromIterable(
+          [
+            initialState,
+            IncidentCreationError(
+              currentOperator: null,
+              cause: 'just testing',
+              stackTrace: StackTrace.current,
+            ),
+          ],
+        ),
+      );
 
-        await Future.delayed(Duration(milliseconds: 100));
-        await tester.pump();
-        expect(find.byType(ErrorDialog), findsOneWidget);
-      });
+      await pumpScreen(tester);
+
+      await tester.pump();
+      expect(find.byType(ErrorDialog), findsOneWidget);
     });
   });
 }
