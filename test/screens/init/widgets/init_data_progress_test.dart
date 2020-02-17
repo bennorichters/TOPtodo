09577ee545fbfcbf1fcf8ed587c11bf09ec32c2f
Benno Richters <bennor@topdesk.com>
2020-02-17 14:01:06 +0100

    using same constants for tests

--- test/screens/init/widgets/init_data_progress_test.dart
+++ test/screens/init/widgets/init_data_progress_test.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:toptodo/blocs/init/bloc.dart';
 import 'package:toptodo/screens/init/widgets/init_data_progress.dart';
-import 'package:toptodo_data/toptodo_data.dart';
 
 import '../../../helper.dart';
 
 void main() {
   group('InitDataProgress', () {
-    const credentials = Credentials(
-      url: 'the url',
-      loginName: 'the loginname',
-      password: 's3cret!',
-    );
-
-    const settings = Settings();
-
-    const currentOperator = TdOperator(
-      id: 'a',
-      name: 'a',
-      firstLine: true,
-      secondLine: true,
-    );
-
     void pumpInitDataProgress(WidgetTester tester, InitData data) async {
       await tester.pumpWidget(
         TestableWidgetWithMediaQuery(
@@ -40,11 +25,11 @@ void main() {
     testWidgets('credentials done', (WidgetTester tester) async {
       await pumpInitDataProgress(
         tester,
-        InitData(credentials: credentials),
+        InitData(credentials: TestConstants.credentials),
       );
 
-      expect(find.text('the url'), findsOneWidget);
-      expect(find.text('the loginname'), findsOneWidget);
+      expect(find.text(TestConstants.credentials.url), findsOneWidget);
+      expect(find.text(TestConstants.credentials.loginName), findsOneWidget);
       expect(find.byIcon(Icons.done), findsOneWidget);
       expect(find.byType(CircularProgressIndicator), findsNWidgets(2));
     });
@@ -52,7 +37,9 @@ void main() {
     testWidgets('credentials and settings done', (WidgetTester tester) async {
       await pumpInitDataProgress(
         tester,
-        InitData(credentials: credentials, settings: settings),
+        InitData(
+            credentials: TestConstants.credentials,
+            settings: TestConstants.settings),
       );
 
       expect(find.byIcon(Icons.done), findsNWidgets(2));
@@ -62,7 +49,9 @@ void main() {
     testWidgets('credentials and operator done', (WidgetTester tester) async {
       await pumpInitDataProgress(
         tester,
-        InitData(credentials: credentials, currentOperator: currentOperator),
+        InitData(
+            credentials: TestConstants.credentials,
+            currentOperator: TestConstants.currentOperator),
       );
 
       expect(find.byIcon(Icons.done), findsNWidgets(2));
@@ -73,9 +62,9 @@ void main() {
       await pumpInitDataProgress(
         tester,
         InitData(
-          credentials: credentials,
-          settings: settings,
-          currentOperator: currentOperator,
+          credentials: TestConstants.credentials,
+          settings: TestConstants.settings,
+          currentOperator: TestConstants.currentOperator,
         ),
       );
 
