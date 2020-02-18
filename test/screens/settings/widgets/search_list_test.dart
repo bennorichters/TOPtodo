1e616e53fbc947ad183ff81eabd52aeb8cd511c2
Benno Richters <bennor@topdesk.com>
2020-02-18 12:39:11 +0100

    tests for SearchList

--- test/screens/settings/widgets/search_list_test.dart
+++ test/screens/settings/widgets/search_list_test.dart
@@ -20,5 +20,45 @@ void main() {
       expect(find.text('name'), findsNothing);
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
+
+    testWidgets('with items', (WidgetTester tester) async {
+      final branchA = TestConstants.branch;
+      final branchB = TdBranch(id: 'b', name: 'b');
+      final branchC = TdBranch(id: 'c', name: 'c');
+
+      await tester.pumpWidget(TestableWidgetWithMediaQuery(
+        child: SearchList<TdBranch>(
+          name: 'name',
+          validationText: 'validation',
+          items: [branchA, branchB, branchC],
+        ),
+      ));
+
+      expect(find.text('name'), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+    });
+
+    testWidgets('with items - nothing selected - show validation text',
+        (WidgetTester tester) async {
+      final branchA = TestConstants.branch;
+      final branchB = TdBranch(id: 'b', name: 'b');
+      final branchC = TdBranch(id: 'c', name: 'c');
+
+      final formKey = GlobalKey<FormState>();
+      await tester.pumpWidget(TestableWidgetWithMediaQuery(
+        child: Form(
+          key: formKey,
+          child: SearchList<TdBranch>(
+            name: 'name',
+            validationText: 'validation',
+            items: [branchA, branchB, branchC],
+          ),
+        ),
+      ));
+
+      expect(formKey.currentState.validate(), isFalse);
+      await tester.pump();
+      expect(find.text('validation'), findsOneWidget);
+    });
   });
 }
