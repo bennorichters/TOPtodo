b3d8ce3399fe64cc9d8ba601a3494d1059c69a03
Benno Richters <b.richters@gmail.com>
2020-02-19 17:26:12 +0100

    more tests for TdModelSearchDelegate

--- test/screens/settings/widgets/td_model_search_delegate_test.dart
+++ test/screens/settings/widgets/td_model_search_delegate_test.dart
@@ -1,10 +1,19 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:toptodo/blocs/td_model_search/bloc.dart';
 import 'package:toptodo/screens/settings/widgets/td_model_search_delegate.dart';
 import 'package:toptodo_data/toptodo_data.dart';
 
 import '../../../helper.dart';
+
+class MockTdModelSearchBloc
+    extends MockBloc<TdModelSearchEvent, TdModelSearchState>
+    implements TdModelSearchBloc {}
 
 void main() {
   group('TdModelSearchDelegate', () {
@@ -37,6 +46,40 @@ void main() {
       expect(back, findsOneWidget);
       await tester.tap(back);
       expect(delegate.closeCalled, isTrue);
+    });
+
+    testWidgets('buildResults empty query', (WidgetTester tester) async {
+      final delegate = _DelegateWrapper.create();
+      await tester.pumpWidget(TestableWidgetWithMediaQuery(
+        child: Builder(
+          builder: (context) => delegate.buildResults(context),
+        ),
+      ));
+
+      expect(find.text('Start typing in the bar at the top'), findsOneWidget);
+    });
+
+    testWidgets('buildResults empty query', (WidgetTester tester) async {
+      TdModelSearchBloc bloc = MockTdModelSearchBloc();
+      when(bloc.state).thenReturn(TdModelSearchInitialState());
+
+      final delegate = _DelegateWrapper.create();
+      delegate.query = 'x';
+
+      await tester.pumpWidget(BlocProvider.value(
+        value: bloc,
+        child: TestableWidgetWithMediaQuery(
+          child: Builder(
+            builder: (context) => delegate.buildResults(context),
+          ),
+        ),
+      ));
+
+      verify(bloc.add(SearchFinishedQuery(
+        linkedTo: null,
+        query: 'x',
+      ))).called(1);
+      expect(find.text('Start typing in the bar at the top'), findsOneWidget);
     });
   });
 }
