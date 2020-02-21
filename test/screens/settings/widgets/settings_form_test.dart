70dda480fa4105861f9eaed2a1db3c06c56c41c9
Benno Richters <bennor@topdesk.com>
2020-02-21 11:57:23 +0100

    test for settingsform

--- test/screens/settings/widgets/settings_form_test.dart
+++ test/screens/settings/widgets/settings_form_test.dart
@@ -1,8 +1,11 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:toptodo/blocs/settings/bloc.dart';
+import 'package:toptodo/blocs/td_model_search/bloc.dart';
 import 'package:toptodo/constants/keys.dart' as ttd_keys;
 import 'package:toptodo/screens/settings/widgets/settings_form.dart';
 
@@ -12,13 +15,19 @@ import '../../../test_constants.dart' as test_constants;
 class MockSettingsBloc extends MockBloc<SettingsEvent, SettingsState>
     implements SettingsBloc {}
 
+class MockTdModelSearchBloc
+    extends MockBloc<TdModelSearchEvent, TdModelSearchState>
+    implements TdModelSearchBloc {}
+
 void main() {
   group('SettingsForm', () {
-    // SettingsBloc bloc;
+    SettingsBloc settingsBloc;
+    TdModelSearchBloc searchBloc;
 
-    // setUp(() {
-    //   bloc = MockSettingsBloc();
-    // });
+    setUp(() {
+      settingsBloc = MockSettingsBloc();
+      searchBloc = MockTdModelSearchBloc();
+    });
 
     testWidgets('basics', (WidgetTester tester) async {
       await tester.pumpWidget(TestableWidgetWithMediaQuery(
@@ -32,15 +41,25 @@ void main() {
     });
 
     testWidgets('tapping', (WidgetTester tester) async {
-      await tester.pumpWidget(TestableWidgetWithMediaQuery(
-        child: SettingsForm(UpdatedSettingsForm(
-          currentOperator: test_constants.currentOperator,
-          formState: SettingsFormState(),
-        )),
+      await tester.pumpWidget(MultiBlocProvider(
+        providers: [
+          BlocProvider.value(value: searchBloc),
+          BlocProvider.value(value: settingsBloc),
+        ],
+        child: TestableWidgetWithMediaQuery(
+          child: SettingsForm(UpdatedSettingsForm(
+            currentOperator: test_constants.currentOperator,
+            formState: SettingsFormState(),
+          )),
+        ),
       ));
 
       final branch = find.byKey(Key(ttd_keys.settingsFormSearchFieldBranch));
-      await tester.tap(branch);
+      final row = find.descendant(of: branch, matching: find.byType(Row));
+      await tester.tap(row);
+
+      verify(searchBloc.add(NewSearch())).called(1);
+      // TODO: test search result
     });
   });
 }
