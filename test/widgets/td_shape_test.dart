ac039ccd28eb31732fc65fc876ba5134a258fcf1
Benno Richters <b.richters@gmail.com>
2020-02-23 11:29:25 +0100

    more tests for TdShape

--- test/widgets/td_shape_test.dart
+++ test/widgets/td_shape_test.dart
@@ -1,9 +1,9 @@
+import 'dart:io';
 import 'dart:ui' as ui;
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:image/image.dart' as img;
+import 'package:image/image.dart' as image_lib;
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:toptodo/constants/colors.dart' as ttd_colors;
@@ -11,30 +11,93 @@ import 'package:toptodo/widgets/td_shape.dart';
 
 void main() {
   group('TdShape', () {
-    testWidgets('pixel right color', (WidgetTester tester) async {
+    testWidgets('long side left', (WidgetTester tester) async {
       await tester.runAsync(() async {
-        final recorder = PictureRecorder();
-        final canvas = Canvas(recorder);
-        final tdShape = TdShape(LongSide.left, ttd_colors.forest100);
-
-        tdShape.paint(canvas, const Size(400, 425));
-
-        final picture = recorder.endRecording();
-        final image = await picture.toImage(400, 425);
-        final bd = await image.toByteData(
-          format: ui.ImageByteFormat.png,
+        final image = await _createImage(
+          width: 400.0,
+          height: 800.0,
+          longSide: LongSide.left,
         );
-        final values = bd.buffer.asUint8List();
-        final myImage = img.decodeImage(values);
 
-        expect(
-          _argbIsAbgr(ttd_colors.forest100, myImage.getPixel(10, 10)),
-          isTrue,
+        isPainted(image, 10, 10);
+        isPainted(image, 10, 770);
+        isNotPainted(image, 390, 10);
+        isNotPainted(image, 390, 770);
+      });
+    });
+
+    testWidgets('long side top', (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        final image = await _createImage(
+          width: 400.0,
+          height: 800.0,
+          longSide: LongSide.top,
         );
+
+        isNotPainted(image, 10, 10);
+        isNotPainted(image, 10, 770);
+        isPainted(image, 390, 10);
+        isNotPainted(image, 390, 770);
+      });
+    });
+
+    testWidgets('long side right', (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        final image = await _createImage(
+          width: 400.0,
+          height: 800.0,
+          longSide: LongSide.right,
+        );
+
+        isNotPainted(image, 10, 10);
+        isNotPainted(image, 10, 770);
+        isPainted(image, 390, 10);
+        isPainted(image, 390, 770);
+      });
+    });
+
+    testWidgets('long side bottom', (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        final image = await _createImage(
+          width: 400.0,
+          height: 800.0,
+          longSide: LongSide.bottom,
+        );
+
+        isNotPainted(image, 10, 10);
+        isNotPainted(image, 10, 770);
+        isNotPainted(image, 390, 10);
+        isPainted(image, 390, 770);
       });
     });
   });
 }
+
+Future<image_lib.Image> _createImage(
+    {double width, double height, LongSide longSide}) async {
+  final recorder = PictureRecorder();
+  final canvas = Canvas(recorder);
+  final tdShape = TdShape(longSide, ttd_colors.forest100);
+
+  tdShape.paint(canvas, Size(width, height));
+
+  final picture = recorder.endRecording();
+  final pictureImage = await picture.toImage(
+    width.floor(),
+    height.floor(),
+  );
+  final byteData = await pictureImage.toByteData(
+    format: ui.ImageByteFormat.png,
+  );
+
+  return image_lib.decodeImage(byteData.buffer.asUint8List());
+}
+
+void isPainted(image_lib.Image image, int x, int y) =>
+    expect(_argbIsAbgr(ttd_colors.forest100, image.getPixel(x, y)), isTrue);
+
+void isNotPainted(image_lib.Image image, int x, int y) =>
+    expect(image.getPixel(x, y), isZero);
 
 bool _argbIsAbgr(Color color, int value) =>
     (color.alpha == _alphaFromAbgr(value)) &&
